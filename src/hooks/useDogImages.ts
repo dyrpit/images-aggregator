964cfc533a62f6dbdabb49b9d555301caad7655f
Piotr Dyrda <piotr.dyrda88@gmail.com>
2024-05-09 10:22:55 +0200

    add dog images hook

--- src/hooks/useDogImages.ts
+++ src/hooks/useDogImages.ts
@@ -1,19 +1,54 @@
+import { useContext, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+
 import { getDogImages } from '../api/services/api.service';
+
+import { ImagesContext } from '../context/images.context';
+
+import { SET_DOG_IMAGES, SET_DOG_LOADING, SET_DOG_LOAD_MORE } from '../state/images/images.actions';
 
 const QUERY_KEY = 'dog_images';
 
 function useDogImages() {
-  const { data, error, isLoading, isError } = useQuery({
+  const { data, error, isLoading, isError, refetch, isRefetching } = useQuery({
     queryKey: [QUERY_KEY],
     queryFn: getDogImages,
   });
+
+  const { dispatch } = useContext(ImagesContext);
+
+  useEffect(() => {
+    if (data) {
+      dispatch({ type: SET_DOG_IMAGES, payload: data });
+    }
+  }, [isLoading, data, dispatch]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error('Failed to fetch cat images');
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (isLoading) {
+      dispatch({ type: SET_DOG_LOADING });
+    }
+  }, [isLoading, dispatch]);
+
+  useEffect(() => {
+    if (isRefetching) {
+      dispatch({ type: SET_DOG_LOAD_MORE });
+    }
+  }, [isRefetching, dispatch]);
 
   return {
     images: data,
     error,
     isLoading,
+    isRefetching,
     isError,
+    refetch,
   };
 }
 
