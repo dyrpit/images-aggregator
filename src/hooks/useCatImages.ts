c87f1aa02653df87c0bfef232963aea69f5a37cf
Piotr Dyrda <piotr.dyrda88@gmail.com>
2024-05-09 10:21:34 +0200

    add cat images hook

--- src/hooks/useCatImages.ts
+++ src/hooks/useCatImages.ts
@@ -1,19 +1,54 @@
+import { useContext, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+
 import { getCatImages } from '../api/services/api.service';
+
+import { ImagesContext } from '../context/images.context';
+
+import { SET_CAT_IMAGES, SET_CAT_LOADING, SET_CAT_LOAD_MORE } from '../state/images/images.actions';
 
 const QUERY_KEY = 'cat_images';
 
 function useCatImages() {
-  const { data, error, isLoading, isError } = useQuery({
+  const { data, error, isLoading, isError, refetch, isRefetching } = useQuery({
     queryKey: [QUERY_KEY],
     queryFn: getCatImages,
   });
+
+  const { dispatch } = useContext(ImagesContext);
+
+  useEffect(() => {
+    if (data) {
+      dispatch({ type: SET_CAT_IMAGES, payload: data });
+    }
+  }, [isLoading, data, dispatch]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error('Failed to fetch cat images');
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (isLoading) {
+      dispatch({ type: SET_CAT_LOADING });
+    }
+  }, [isLoading, dispatch]);
+
+  useEffect(() => {
+    if (isRefetching) {
+      dispatch({ type: SET_CAT_LOAD_MORE });
+    }
+  }, [isRefetching, dispatch]);
 
   return {
     images: data,
     error,
     isLoading,
+    isRefetching,
     isError,
+    refetch,
   };
 }
 
