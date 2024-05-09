b67db5a342872bd2b6b3c9be357a8f284a3b88f2
Piotr Dyrda <piotr.dyrda88@gmail.com>
2024-05-09 22:40:20 +0200

    fix base url to be different based on NODE_ENV, update script, install types for node, set fixed port for dev preview

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const baseURL = process.env.NODE_ENV === 'production' ? '/images-aggregator' : '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/images-aggregator',
+  base: baseURL,
+  preview: {
+    port: 5173,
+  },
   plugins: [react()],
 });
